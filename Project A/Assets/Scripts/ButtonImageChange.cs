ac8ca4b6f3ebbde97975eda4ee51d8d1ab1ac972
IsaacBernardes <51682756+IsaacBernardes@users.noreply.github.com>
2020-01-08 14:10:02 -0300

    Botao de configurações funcionando e o meu opções também (Organizei um pouco pq arthur bagunçou

---
+++ Project A/Assets/Scripts/ButtonImageChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonImageChange : MonoBehaviour
+{
+    public Sprite Imagem1, Imagem2;
+    public Button botao;
+    private bool ativo = true;
+
+    public void trocar()
+    {
+        
+        if (ativo)
+        {
+            botao.image.sprite = Imagem2;
+            ativo = false;
+        }
+        else
+        {
+            botao.image.sprite = Imagem1;
+            ativo = true;
+        }
+    }
+
+
+}
+
